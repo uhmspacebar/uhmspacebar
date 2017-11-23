4a86893de1830e5ed9c427bfdd5b1bd9e9c77e35
Paulo Lemus <paulolemus14@gmail.com>
2017-11-22 22:17:25 -1000

    Added page mockup layout

---
+++ app/imports/ui/pages/siteadmin/index.js
@@ -0,0 +1,3 @@
+import './site-admin.html';
+import './site-admin.css';
+import './site-admin.js';
---
+++ app/imports/ui/pages/siteadmin/site-admin.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import './site-admin.html'
+
+
+
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
+
+
+Template.Site_Admin_Page.onCreated(function onCreated() {
+
+});
+
+Template.Site_Admin_Page.onRendered(function () {
+
+});
+
+Template.Site_Admin_Page.helpers({
+
+});
+
+Template.Site_Admin_Page.events({
+
+});
+
+
