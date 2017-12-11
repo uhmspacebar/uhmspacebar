5b10f2511534194c035c3b7bf0e28b83eab8c9e6
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-12-11 10:06:11 -1000

    added username display on logout button

--- app/imports/ui/layouts/user/user-header.js
+++ app/imports/ui/layouts/user/user-header.js
@@ -6,3 +6,35 @@ Template.User_Header.helpers({
     return FlowRouter.getParam('username');
   },
 });
+
+Template.User_Header.onRendered(function onRendered() {
+  this.$('.dropdown').dropdown();
+});
+Template.User_Header.events({
+  /**
+   * Handle the click on the logout link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-logout': function casLogout(event) {
+    event.preventDefault();
+    Meteor.logout();
+    return false;
+  },
+
+  /**
+   * Handle the click on the login link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-login': function casLogin(event) {
+    event.preventDefault();
+    const callback = function loginCallback(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+    Meteor.loginWithCas(callback);
+    return false;
+  },
+});
