2ce90e545c939eef5736127066a35c566bba72f9
chokevin <cap.kevincho@gmail.com>
2017-12-13 14:36:13 -1000

    changed new user

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -1,5 +1,6 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Clubs } from '/imports/api/club/ClubCollection';
 
 /* eslint-disable no-console */
 
@@ -9,6 +10,7 @@ Accounts.validateNewUser(function validate(user) {
     const username = user.services.cas.id;
     if (!Profiles.isDefined(username)) {
       Profiles.define({ username });
+      Clubs.define({ username });
     }
   }
   // All UH users are valid for BowFolios.
